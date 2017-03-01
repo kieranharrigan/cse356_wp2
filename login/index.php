fba086797180933c6ca8eddac3313c62e615b6b2
Kieran <kieranharrigan@gmail.com>
2017-03-01 17:10:44 -0500

    Modified formatting.

--- login/index.php
+++ login/index.php
@@ -5,30 +5,33 @@ $password = $fields['password'];
 
 if ($username === NULL || $password === NULL) {
     $phrase = 'Incorrect usage of /login.';
-} else {
-        $db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
-        $query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
-        $db->exec($query);
-        
-        $result = $db->query("SELECT * FROM users WHERE lower(username) = '" . strtolower($username) . "'");
-        $exists = $result->fetchArray();
-        
-        if ($exists) {
-            if ($exists['disabled']) {
-                $phrase = 'ERROR';
-            } else {
-                if(strcmp($exists['password'], $password) === 0) {
-                    $phrase = 'OK';
-                    session_start();
-                    $_SESSION['username'] = $username;
-                }
-                else {
-                    $phrase = 'ERROR';
-                }
-            }
-        } else {
+}
+else {
+    $db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
+    $query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
+    $db->exec($query);
+
+    $result = $db->query("SELECT * FROM users WHERE lower(username) = '" . strtolower($username) . "'");
+    $exists = $result->fetchArray();
+
+    if ($exists) {
+        if ($exists['disabled']) {
             $phrase = 'ERROR';
         }
+        else {
+            if(strcmp($exists['password'], $password) === 0) {
+                $phrase = 'OK';
+                session_start();
+                $_SESSION['username'] = $username;
+            }
+            else {
+                $phrase = 'ERROR';
+            }
+        }
+    }
+    else {
+        $phrase = 'ERROR';
+    }
 }
 
 $response = array("status" => $phrase);
