18921015fa10338eebbf478cee25c91befce8d82
Kieran <kieranharrigan@gmail.com>
2017-03-01 18:19:45 -0500

    Added /login front end.

--- login/index.php
+++ login/index.php
@@ -1,41 +1,16 @@
-<?php
-$fields = json_decode(file_get_contents('php://input'), true);
-$username = $fields['username'];
-$password = $fields['password'];
+<html>
+<head>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="login.js"></script>
+</head>
 
-if ($username === NULL || $password === NULL) {
-    $phrase = 'Incorrect usage of /login.';
-}
-else {
-    $db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
-    $query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
-    $db->exec($query);
+<body>
+    <form id="input" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off">
+        Username: <input type="text" name="username" autofocus><br>
+        Password: <input type="text" name="password"><br>
+        <input type="submit" value="submit">
+    </form>
 
-    $result = $db->query("SELECT * FROM users WHERE lower(username) = '" . strtolower($username) . "'");
-    $exists = $result->fetchArray();
-
-    if ($exists) {
-        if ($exists['disabled']) {
-            $phrase = 'ERROR';
-        }
-        else {
-            if(strcmp($exists['password'], $password) === 0) {
-                $phrase = 'OK';
-                session_start();
-                $_SESSION['username'] = $username;
-            }
-            else {
-                $phrase = 'ERROR';
-            }
-        }
-    }
-    else {
-        $phrase = 'ERROR';
-    }
-}
-
-$response = array("status" => $phrase);
-$json = json_encode($response);
-
-echo $json;
-?>
+    <div id="result"></div>
+</body>
+</html>
---
+++ login/login.js
@@ -0,0 +1,17 @@
+function passToAdd() {
+	var arr = $("#input").serializeArray();
+	var json = {};
+
+	$.each(arr, function() {
+		json[this.name] = this.value;
+	});
+
+	$.ajax({
+		url: "login.php/",
+		type: "POST",
+		data: JSON.stringify(json),
+		success: function(reply) {
+			document.getElementById("result").innerHTML = reply;       
+		}
+	});
+}
