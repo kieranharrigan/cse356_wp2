fef0ff2d2eb376a531c278ecc565e0c043c6645e
Kieran <kieranharrigan@gmail.com>
2017-03-02 16:57:06 -0500

    Swapped files.

--- verify/verify.php
+++ verify/verify.php
@@ -1,45 +1,38 @@
-<?php
-$fields = json_decode(file_get_contents('php://input'), true);
-$email = $fields['email'];
-$key = $fields['key'];
+<html>
+<head>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+</head>
 
-if ($email === NULL || $key === NULL) {
-    $phrase = 'Incorrect usage of /verify.';
-}
-else {
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $phrase = 'Invalid email address.';
-    }
-    else {
-        $db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
-        $query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
-        $db->exec($query);
-        
-        $result = $db->query("SELECT * FROM users WHERE lower(email) = '" . strtolower($email) . "'");
-        $exists = $result->fetchArray();
-        
-        if ($exists) {
-            if ($exists['disabled']) {
-                if (strcmp($exists['key'], $key) === 0 || strcmp($key, 'abracadabra') === 0) {
-                    $db->exec("UPDATE users SET disabled = 0 WHERE lower(email) = '" . strtolower($email) . "'");
-                    $phrase = $exists['username'] . ' verified successfully.';
-                }
-                else {
-                    $phrase = 'Incorrect email/key.';
-                }
-            }
-            else {
-                $phrase = $exists['username'] . ' is already verified.';
-            }
-        }
-        else {
-            $phrase = 'No existing user with email, ' . $email;
-        }
-    }
-}
+<body>
+	<div id="result"></div>
 
-$response = array("phrase" => $phrase);
-$json = json_encode($response);
+	<script>
+		function verify() {
+			var email = getUrlParameter('email');
+			var key = getUrlParameter('key');
 
-echo $json;
-?>
+			var json = '{"email":"' + email + '", "key":"' + key + '"}';
+
+			$.ajax({
+				url: "/var/www/html/verify/verify.php",
+				type: "POST",
+				data: json,
+				dataType: "json",
+				success: function(reply) {
+					document.getElementById("result").innerHTML += reply.phrase + "<br>";
+				}  
+			});
+		}
+
+		function getUrlParameter(name) {
+			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+			var results = regex.exec(location.search);
+			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+		}
+
+		verify();
+	</script>
+
+</body>
+</html>
