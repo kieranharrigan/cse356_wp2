39377fcce0b9edb26b29b25262a7987bf28c33c0
Kieran <kieranharrigan@gmail.com>
2017-03-02 18:49:12 -0500

    Reworked login.

--- login/index.php
+++ login/index.php
@@ -39,7 +39,7 @@ else :
 <html>
 <head>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="login.js"></script>
+	<script type="text/javascript" src="/login/login.js"></script>
 </head>
 
 <body>
