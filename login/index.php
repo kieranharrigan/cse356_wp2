b36cc26130afd6f3a678de3258f7338fbb3dc3cf
Kieran <kieranharrigan@gmail.com>
2017-03-02 01:45:35 -0500

    Renamed files.

--- login/index.php
+++ login/index.php
@@ -3,57 +3,39 @@ $fields = json_decode(file_get_contents('php://input'), true);
 $username = $fields['username'];
 $password = $fields['password'];
 
-if ($username !== NULL && $password !== NULL) :
-	$db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
-	$query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
-	$db->exec($query);
+if ($username === NULL || $password === NULL) {
+    $phrase = 'Incorrect usage of /login.';
+}
+else {
+    $db = new SQLite3('/var/www/html/databases/eliza_db.sqlite');
+    $query = 'CREATE TABLE IF NOT EXISTS users (username STRING PRIMARY KEY, password STRING, email STRING, key STRING, disabled INTEGER)';
+    $db->exec($query);
 
-	$result = $db->query("SELECT * FROM users WHERE lower(username) = '" . strtolower($username) . "'");
-	$exists = $result->fetchArray();
+    $result = $db->query("SELECT * FROM users WHERE lower(username) = '" . strtolower($username) . "'");
+    $exists = $result->fetchArray();
 
-	if ($exists) {
-		if ($exists['disabled']) {
-			$phrase = 'ERROR';
-		}
-		else {
-			if(strcmp($exists['password'], $password) === 0) {
-				$phrase = 'OK';
-				session_start();
-				$_SESSION['username'] = $username;
-			}
-			else {
-				$phrase = 'ERROR';
-			}
-		}
-	}
-	else {
-		$phrase = 'ERROR';
-	}
+    if ($exists) {
+        if ($exists['disabled']) {
+            $phrase = 'ERROR';
+        }
+        else {
+            if(strcmp($exists['password'], $password) === 0) {
+                $phrase = 'OK';
+                session_start();
+                $_SESSION['username'] = $username;
+            }
+            else {
+                $phrase = 'ERROR';
+            }
+        }
+    }
+    else {
+        $phrase = 'ERROR';
+    }
+}
 
-	$response = array("status" => $phrase);
-	$json = json_encode($response);
+$response = array("status" => $phrase);
+$json = json_encode($response);
 
-	echo $json;
-else :
-?>
-
-<html>
-<head>
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-	<script type="text/javascript" src="login.js"></script>
-</head>
-
-<body>
-	<form id="input" onsubmit="event.preventDefault(); passToAdd();" autocomplete="off">
-		Username: <input type="text" name="username" autofocus><br>
-		Password: <input type="text" name="password"><br>
-		<input type="submit" value="submit">
-	</form>
-
-	<div id="result"></div>
-</body>
-</html>
-
-<?php
-endif;
+echo $json;
 ?>
