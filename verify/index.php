6ee4ca8bfec42ca23cfd8455c790ba086a50dae1
Kieran <kieranharrigan@gmail.com>
2017-03-02 16:47:06 -0500

    Added autoverify to index.

--- verify/index.php
+++ verify/index.php
@@ -1,13 +1,38 @@
 <html>
 <head>
-	<script type="text/javascript" src="autoverify.js"></script>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 </head>
 
 <body>
 	<div id="result"></div>
 
-	<script>verify();</script>
+	<script>
+		function verify() {
+			var email = getUrlParameter('email');
+			var key = getUrlParameter('key');
+
+			var json = '{"email":"' + email + '", "key":"' + key + '"}';
+
+			$.ajax({
+				url: "/verify/verify.php/",
+				type: "POST",
+				data: json,
+				dataType: "json",
+				success: function(reply) {
+					document.getElementById("result").innerHTML += reply.phrase + "<br>";
+				}  
+			});
+		}
+
+		function getUrlParameter(name) {
+			name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+			var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+			var results = regex.exec(location.search);
+			return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+		}
+
+		verify();
+	</script>
 
 </body>
 </html>
