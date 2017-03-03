7728bb80edf424dfeba1ba69176252cac29eae21
Kieran <kieranharrigan@gmail.com>
2017-03-02 23:16:41 -0500

    Final for now.

--- DOCTOR/index.php
+++ DOCTOR/index.php
@@ -39,7 +39,7 @@ $convs_db->exec($query);
 
 $json = json_encode($response);
 
-echo $json . PHP_EOL . $query;
+echo $json;
 
 function phrase($human) {
   $human = preg_replace('/\s+/', '', $human);
