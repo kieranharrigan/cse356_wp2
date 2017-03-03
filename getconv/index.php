9dc0bbbd438fd222cc816c3db71d64df886d099f
Kieran <kieranharrigan@gmail.com>
2017-03-02 21:47:22 -0500

    New test.

--- getconv/index.php
+++ getconv/index.php
@@ -10,18 +10,16 @@ if($_SESSION['username'] !== NULL) {
 	$phrase = 'OK';
 
 	$single = new SQLite3('/var/www/html/databases/conv.sqlite');
-	$query = 'CREATE TABLE IF NOT EXISTS conv (id STRING PRIMARY KEY, timestamp STRING, name STRING, text STRING)';
+	$query = 'CREATE TABLE IF NOT EXISTS conv (id STRING, timestamp STRING, name STRING, text STRING)';
 	$single->exec($query);
 
 	$result = $single->query("SELECT * FROM conv WHERE lower(id) = '" . strtolower($id) . "'");
 
-	//$exists = $result->fetchAll();
+	$exists = $result->fetchAll();
 
-	foreach($result as $row){
-		$exists = $row->fetchArray();
-		
-        array_push($convos, array("timestamp" => $exists['timestamp'], "name" => $exists['name'], "text" => $exists['text']));
-    }
+	while($exists = $result->fetchArray()) {
+		array_push($convos, array("timestamp" => $exists['timestamp'], "name" => $exists['name'], "text" => $exists['text']));
+	}
 }
 
 $response = array("status" => $phrase, "conversation" => $convos);
