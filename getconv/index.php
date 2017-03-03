07b39b52f8816fb2c10ae7cd64732c0e470c5cbd
Kieran <kieranharrigan@gmail.com>
2017-03-02 21:34:36 -0500

    Using fetchAll().

--- getconv/index.php
+++ getconv/index.php
@@ -4,21 +4,22 @@ $fields = json_decode(file_get_contents('php://input'), true);
 $id = $fields['id'];
 
 $phrase = 'ERROR';
+$convos = array();
 
 if($_SESSION['username'] !== NULL) {
 	$phrase = 'OK';
-}
 
-$single = new SQLite3('/var/www/html/databases/conv.sqlite');
-$query = 'CREATE TABLE IF NOT EXISTS conv (id STRING PRIMARY KEY, timestamp STRING, name STRING, text STRING)';
-$single->exec($query);
+	$single = new SQLite3('/var/www/html/databases/conv.sqlite');
+	$query = 'CREATE TABLE IF NOT EXISTS conv (id STRING PRIMARY KEY, timestamp STRING, name STRING, text STRING)';
+	$single->exec($query);
 
-$result = $single->query("SELECT * FROM conv WHERE lower(id) = '" . strtolower($id) . "'");
+	$result = $single->query("SELECT * FROM conv WHERE lower(id) = '" . strtolower($id) . "'");
 
-$convos = array();
+	$exists = $result->fetchAll();
 
-while($exists = $result->fetchArray()) {
-	array_push($convos, array("timestamp" => $exists['timestamp'], "name" => $exists['name'], "text" => $exists['text']));
+	foreach($exists as $row){
+        array_push($convos, array("timestamp" => $row['timestamp'], "name" => $row['name'], "text" => $row['text']));
+    }
 }
 
 $response = array("status" => $phrase, "conversation" => $convos);
